bd69ecd27f8669bfd3ea03a73dd4e8452c6aa8a1
Lx <yanaski@msn.com>
2020-05-08 17:36:53 +0200

    Fixing SetChildren after type extensions changes

--- Code/Library/Nodes - Switch/SwitchDefaultNode.cs
+++ Code/Library/Nodes - Switch/SwitchDefaultNode.cs
@@ -25,16 +25,25 @@ namespace FlowChartCore
 
         
         internal override void SetChildren() {
-            
+
             // On appelle GetChildAST qui est une extension pour le type
             // Ca nous retourne une liste d'AST
             IEnumerable<Ast> Childs = RawAst.GetChildAst();
             
             int p = 0;
+            bool tmp = true;
+
             foreach (var item in Childs)
             {
-                // On appelle CreateNode qui est une extension pour AST
-                children.Add(item.CreateNode(depth+1,p,this,null));
+                if (tmp && !FlowChartCore.Utility.GetValidTypes().Contains(item.GetType()) ) {
+                    children.Add(new CodeNode(depth+1,p,this,null));
+                    tmp = false;
+                }
+                else if(FlowChartCore.Utility.GetValidTypes().Contains(item.GetType())){
+                    // On appelle CreateNode qui est une extension pour AST
+                    children.Add(item.CreateNode(depth+1,p,this,null));
+                    tmp = true;
+                }
                 p++;
             }
         }
