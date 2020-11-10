dd65d7ba25b71b738e8db522f4230052fac2afdf
LxLeChat <yanaski@msn.com>
2020-11-10 23:06:00 +0100

    ALLAY

--- Test/UseCases.Tests.ps1
+++ Test/UseCases.Tests.ps1
@@ -28,7 +28,7 @@ If ( $null -eq $ExcludeCase)
 [string[]] $ExcludeCase=$ExcludeCase
 
 #FlowChartCore init
-$ModulePath="$PSScriptRoot\..\Src\bin\Debug\netcoreapp3.1"
+$ModulePath="..\Src\bin\Debug\netcoreapp3.1"
 
 # unblock-File "$ModulePath\DotNetGraph.dll"
 # unblock-File "$ModulePath\FlowchartCore.dll"
@@ -52,7 +52,7 @@ param(
 }
 
 # Build use cases
-$UseCasesPath="$PSScriptRoot\..\Test\Dev"
+$UseCasesPath="..\Test\Dev"
 Write-Host "`n`rAdd use cases from :`r`n$UseCasesPath"
 
 $CodeUseCases=Get-Childitem $UseCasesPath |
