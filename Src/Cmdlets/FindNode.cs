19319417fd089b251dd627f0828a4df32af646f3
LxLeChat <yanaski@msn.com>
2020-12-13 16:22:36 +0100

    Resolve Issue #15 and #93 Find-Flow.. ->  Get-Flow..

--- Src/Cmdlets/FindNode.cs
+++ Src/Cmdlets/FindNode.cs
@@ -4,52 +4,78 @@ using System.Collections.Generic;
 
 namespace FlowChartCore.Cmdlets {
 
-    // Find-FlowChartNodes, will help find "nodes" in a PS1 script
-    [Cmdlet(VerbsCommon.Find,"FLowChartNodes")]
+    // Get-FlowChartNode, will return "nodes" from a PS1 script or a scriptblock
+    [Cmdlet(VerbsCommon.Get,"FLowChartNode",DefaultParameterSetName = PathParameterSet)]
     [OutputType(typeof(Node))]
-    public class FindFlowChartNodes : PSCmdlet
+    public class GetFlowChartNodes : PSCmdlet
     {
         [Parameter(
             Mandatory = true,
-            Position = 0,
-            ValueFromPipeline = false,
-            ValueFromPipelineByPropertyName = true,
-            ParameterSetName = "Script")]
+            ParameterSetName = ScriptBlockParameterSet
+        )]
         [ValidateNotNullAttribute]
         public ScriptBlock ScriptBlock { get; set; }
 
-        private String[] _paths;
-        private Boolean  _wildcards;
-
+        /// <summary>
+        /// Path parameter.
+        /// The paths of the files.
+        /// Resolved wildcards.
+        /// </summary>
+        /// <value></value>
         [Parameter(
-            ParameterSetName="Path",
-            Mandatory=true,
-            Position=0,
-            ValueFromPipeline=true,
-            ValueFromPipelineByPropertyName=true
+            Mandatory = true,
+            ParameterSetName = PathParameterSet,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
         )]
-        public String[] Path {
-            get { return _paths; }
-            set {
-                _wildcards = true;
+        public string[] Path
+        {
+            get
+            {
+                return _paths;
+            }
+
+            set
+            {
                 _paths = value;
             }
         }
 
+        /// <summary>
+        /// LiteralPath parameter.
+        /// The literal paths of the files.
+        /// Don't resolved wildcards.
+        /// </summary>
+        /// <value></value>
         [Parameter(
-            ParameterSetName="Path",
-            Mandatory=false,
-            Position=0,
-            ValueFromPipeline=true,
-            ValueFromPipelineByPropertyName=true
+            Mandatory = true,
+            ParameterSetName = LiteralPathParameterSet,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true
         )]
-        [Alias("PSPath")]
-        public String[] LiteralPath {
-            get { return _paths; }
-            set { _paths = value; }
+        [Alias("PSPath", "LP")]
+        public string[] LiteralPath
+        {
+            get
+            {
+                return _paths;
+            }
+
+            set
+            {
+                _paths = value;
+            }
         }
 
+        private string[] _paths;
 
+        /// <summary>
+        /// Parameter set names.
+        /// </summary>
+        private const string PathParameterSet = "Path";
+        private const string LiteralPathParameterSet = "LiteralPath";
+        private const string ScriptBlockParameterSet = "ScriptBlock";
 
         // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
         protected override void BeginProcessing()
@@ -60,14 +86,13 @@ namespace FlowChartCore.Cmdlets {
         protected override void ProcessRecord()
         {
             List<Node> ListOfNodes = new List<Node>();
+            ProviderInfo pi;
             switch (this.ParameterSetName)
             {
-                case "Path":
-                    ProviderInfo pi;
-
+                case PathParameterSet:
                     foreach (var item in _paths)
                     {
-                        String file = _wildcards ? this.SessionState.Path.GetResolvedProviderPathFromPSPath(item, out pi)[0] : this.SessionState.Path.GetUnresolvedProviderPathFromPSPath(item);
+                        String file = this.SessionState.Path.GetResolvedProviderPathFromPSPath(item, out pi)[0];
                         ListOfNodes =  FlowChartCore.Utility.ParseFile(file);
                         if (ListOfNodes.Count > 0 )
                         {
@@ -76,7 +101,19 @@ namespace FlowChartCore.Cmdlets {
 
                     }
                     break;
-                case "Script":
+                case LiteralPathParameterSet :
+                    foreach (var item in _paths)
+                    {
+                        String file = this.SessionState.Path.GetResolvedProviderPathFromPSPath(item, out pi)[0];
+                        ListOfNodes =  FlowChartCore.Utility.ParseFile(file);
+                        if (ListOfNodes.Count > 0 )
+                        {
+                            WriteObject(ListOfNodes);   
+                        }
+
+                    }
+                    break;
+                case ScriptBlockParameterSet:
                     // Changing behavior, for example -scriptblocl {} return 0 nodes. We Want to return 0, not an empty List.
                     ListOfNodes =  FlowChartCore.Utility.ParseScriptBlock(ScriptBlock);
                     if (ListOfNodes.Count > 0 )
@@ -87,6 +124,7 @@ namespace FlowChartCore.Cmdlets {
                 default:
                     break;
             }
+        
         }
 
         // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
