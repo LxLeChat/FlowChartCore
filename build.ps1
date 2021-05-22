8bd462462957b3e341de28aa3503eb4b67e77dc6
LxLeChat <yanaski@msn.com>
2021-05-22 20:30:56 +0200

    Digraph Names

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
-[CmdletBinding()]
 param (
-    [Parameter()]
-    [TypeName]
-    $ParameterName
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $OfflineBuild,
+    [Parameter(Mandatory=$False)]
+    [ValidateSet]
+    $TestPlateform = ("PS7","PS5")
 )
+
+
 $Messages = @{
     DotNetBuild = "Building Project..."
     BuildSuccessfull = "Build Successfull.."
@@ -13,21 +17,41 @@ $Messages = @{
 }
 
 Write-Information -MessageData $Messages.DotNetBuild -InformationAction Continue
+
+
+## build offline, configuration offline packages & config file
 Set-Location -Path .\Src
+remove-item .\Packages\* -Recurse
+Expand-Archive .\resources\Packages.zip -DestinationPath .\Packages
+[xml]$NugetConfig = Get-Content .\packages\nuget.config
+$NugetConfig.configuration.packageSources.add.value = "$($(Get-Item .\packages\nuget.config).Directory)"
+$NugetConfig.Save($(get-item .\packages\nuget.config).FullName)
+
+## cleaning dotnet output directories
+# dotnet clean
+
+## cleaning Module directory
+Gci ..\Module -Recurse | Remove-Item
+
+## dotnet build
 $Build = dotnet build
+set-location ..
 
 ## was build successfull ?
 If ( $? ) {
     Write-Information $Messages.BuildSuccessfull -InformationAction Continue
 
     Write-Information $Messages.ImportingModule -InformationAction Continue
-    Set-Location -Path .\bin\debug\netcoreapp3.1\
-    Import-Module .\FlowchartCore.dll -ErrorAction Stop
+    Import-Module .\src\bin\debug\netcoreapp3.1\FlowchartCore.dll -ErrorAction Stop
 
     Write-Information $Messages.RunningTests -InformationAction Continue
-    Set-Location -Path ..\..\..\..\Test
+    # Set-Location -Path ..\Test
+
     Import-Module -Name Pester -ErrorAction Stop
-    Invoke-Pester -Path  .\UsesCases.Tests.ps1 -Output Detailed
+    Set-Location .\Test
+    # Invoke-Pester -path .\test\UsesCases.Tests.ps1 -Output Detailed -ErrorAction stop
+    Invoke-Pester
+    
 } Else {
     Write-Information $Messages.BuildFailed -InformationAction Continue
     Throw "Build Failed..."
